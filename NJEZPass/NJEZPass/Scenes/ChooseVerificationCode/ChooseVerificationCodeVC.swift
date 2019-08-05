556f1d91f19969dd4a209eec85bbc99e3d231b99
Narasimha <>
2019-08-05 15:06:05 +0530

    choose verification vc added

---
+++ NJEZPass/NJEZPass/Scenes/ChooseVerificationCode/ChooseVerificationCodeVC.swift
@@ -0,0 +1,31 @@
+//
+//  ChooseVerificationCodeVC.swift
+//  NJEZPass
+//
+//  Created by N, Narasimhulu on 05/08/19.
+//  Copyright © 2019 Conduent. All rights reserved.
+//
+
+import UIKit
+
+class ChooseVerificationCodeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.navigationItem.addNavBarImage()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
