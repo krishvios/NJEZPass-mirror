0afd57177a4c9a6515b73daf8e775ed5efc8a0db
Narasimha <>
2019-08-08 15:06:34 +0530

    updated the flow

---
+++ NJEZPass/NJEZPass/Scenes/Login/LoginVC.swift
@@ -0,0 +1,31 @@
+//
+//  LoginVC.swift
+//  NJEZPass
+//
+//  Created by N, Narasimhulu on 07/08/19.
+//  Copyright © 2019 Conduent. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: false)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+    @IBAction func backTapped(_ sender: Any) {
+        self.navigationController?.popToRootViewController(animated: true)
+    }
+}
