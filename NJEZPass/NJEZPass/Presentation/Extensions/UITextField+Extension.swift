64c6b0674410a304324d49a7b415a8c2015bb8ac
Narasimha <>
2019-08-12 16:12:16 +0530

    updated the verification code auto layouts

--- NJEZPass/NJEZPass/Presentation/Extensions/UITextField+Extension.swift
+++ NJEZPass/NJEZPass/Presentation/Extensions/UITextField+Extension.swift
@@ -9,13 +9,14 @@
 import UIKit
 
 extension UITextField {
-    func addBottomBorder() {
+    func addBottomBorder(layerWidth: CGFloat) {
         let border = CALayer()
         let width = CGFloat(1.0)
         border.borderColor = UIColor.lightGray.cgColor
-        border.frame = CGRect(x: 0, y: self.frame.size.height - width, width:  self.frame.size.width, height: self.frame.size.height)
+        border.frame = CGRect(x: 2, y: self.frame.size.height - width, width: layerWidth, height: self.frame.size.height)
         border.borderWidth = width
         self.layer.addSublayer(border)
         self.layer.masksToBounds = true
     }
 }
+
