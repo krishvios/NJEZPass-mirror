5bc6576da9ecccc21053107eb1e76ff8a8ec52a4
Narasimha <>
2019-08-12 17:56:16 +0530

    Logout navigation added

--- NJEZPass/NJEZPass/Presentation/Modules/Home/SideMenuTableViewController.swift
+++ NJEZPass/NJEZPass/Presentation/Modules/Home/SideMenuTableViewController.swift
@@ -32,4 +32,32 @@ class SideMenuTableViewController: UITableViewController {
         return cell
     }
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+       
+        let selectedCell:UITableViewCell = tableView.cellForRow(at: indexPath)!
+        selectedCell.contentView.backgroundColor = UIColor(red: 0/255.0, green: 0/255.0, blue: 0/255.0, alpha: 0.05)
+        
+        if indexPath.row == 8 {
+            let actionSheetController = UIAlertController(title: "", message: "Are you sure you want to log out of the app?", preferredStyle: .actionSheet)
+            
+            guard let delegate = UIApplication.shared.delegate as? AppDelegate  else {
+                return
+            }
+            
+            let signOutActionButton = UIAlertAction(title: "Log Out", style: .default) { action in
+                let storyBoard = UIStoryboard(name: "UserFlow", bundle: nil)
+                let vc = storyBoard.instantiateViewController(withIdentifier: "LandingViewControllercvbcxzsdrtyutrdsa") as! LandingViewController
+                delegate.window?.rootViewController = UINavigationController(rootViewController: vc)
+                
+            }
+            actionSheetController.addAction(signOutActionButton)
+            let cancelActionButton = UIAlertAction(title: "Cancel", style: .destructive) { action -> Void in
+                actionSheetController.view.tintColor = UIColor.red
+            }
+            actionSheetController.addAction(cancelActionButton)
+            
+            self.present(actionSheetController, animated: true, completion: nil)
+        }
+    }
+    
 }
