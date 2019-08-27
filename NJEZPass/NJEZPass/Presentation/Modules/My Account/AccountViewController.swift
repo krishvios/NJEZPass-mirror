64fa1010f7928fd70d242d2bd37b218f08900a80
Narasimha <>
2019-08-27 12:26:25 +0530

    Updated the flow

--- NJEZPass/NJEZPass/Presentation/Modules/My Account/AccountViewController.swift
+++ NJEZPass/NJEZPass/Presentation/Modules/My Account/AccountViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class AccountViewController: UIViewController {
     
-    let rowItems = ["Edit Profile","Account Balance","Payment Methods","Account Activity","Settings"]
+    let rowItems = ["Edit Profile","Payment Methods","Vehicles","Tags"]
 
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
@@ -24,24 +24,6 @@ class AccountViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         self.navigationController?.navigationBar.barTintColor = .white
     }
-    
-    @IBAction func homeButtonAction(_ sender: Any) {
-        
-        let storyboard = UIStoryboard(name: "UserFlow", bundle: nil)
-        let vc = storyboard.instantiateViewController(withIdentifier: "DashboardView")
-        self.present(vc, animated: false, completion: nil)
-    }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 
 extension AccountViewController: UITableViewDataSource, UITableViewDelegate {
@@ -66,20 +48,10 @@ extension AccountViewController: UITableViewDataSource, UITableViewDelegate {
             self.performSegue(withIdentifier: "EditProfile", sender: nil)
             break;
         case 1:
-            self.performSegue(withIdentifier: "AccountBal", sender: nil)
-            break;
-        case 2:
             self.performSegue(withIdentifier: "PaymentMethods", sender: nil)
-            break;
-        case 3:
-            self.performSegue(withIdentifier: "AccountActivity", sender: nil)
-            break;
-        case 4:
-            self.performSegue(withIdentifier: "Settings", sender: nil)
             break;
         default:
             break;
         }
     }
-     
 }
