d94b0ef201645c6f37cc010e636be40fc50e0bbc
A N S Krishna Vivek <vivek.amirapu@conduent.com>
2019-09-04 12:23:59 +0530

    Transactions View Controller Added.

--- NJEZPass/NJEZPass/Presentation/Modules/Transcations/TransactionsViewController.swift
+++ NJEZPass/NJEZPass/Presentation/Modules/Transcations/TransactionsViewController.swift
@@ -8,22 +8,65 @@
 
 import UIKit
 
-class TransactionsViewController: UIViewController {
-
+class TransactionsViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tbleView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        setupTableView()
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setupTableView(){
+        //        let nib = UINib(nibName: String(describing: ButtonTableViewCell.self), bundle: nil)
+        //        tbleView.register(nib, forCellReuseIdentifier: String(describing: ButtonTableViewCell.self))
+        tbleView.estimatedRowHeight = 100
+        tbleView.rowHeight = UITableView.automaticDimension
+        tbleView.keyboardDismissMode = .onDrag
+        
     }
-    */
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 63
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let cellIdentifier = "SectionHeader"
+        let headerView = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
+        if headerView == nil {
+//            [NSException raise:@"headerView == nil.." format:@"No cells with matching CellIdentifier loaded from your storyboard"];
+//            NSException.raise(NSExceptionName(rawValue: "headerView == nil.."), format: "No cells with matching CellIdentifier", arguments: ...)
+        }
+        return headerView
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cellIdentifier = "TransactionCell"
+        
+//        switch indexPath.row {
+//
+//        case 0:
+//            cellIdentifier = "PaymentInformation"
+//        case 1:
+//            cellIdentifier = "ReplenishmentMethod"
+//        default:
+//            cellIdentifier = "ReplenishmentOption"
+//        }
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
+        
+        return cell!
+    }
 
 }
