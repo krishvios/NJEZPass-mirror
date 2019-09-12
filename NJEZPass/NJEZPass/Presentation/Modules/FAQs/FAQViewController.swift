64696adc81b19b5c4022d63d563d26b0147555a0
A N S Krishna Vivek <vivek.amirapu@conduent.com>
2019-09-12 17:40:28 +0530

    Added Search Controller.

--- NJEZPass/NJEZPass/Presentation/Modules/FAQs/FAQViewController.swift
+++ NJEZPass/NJEZPass/Presentation/Modules/FAQs/FAQViewController.swift
@@ -15,28 +15,54 @@ struct ViewTags {
     static let ansCoantinerT = 14
 }
 
-class FAQViewController: UITableViewController {
+class FAQViewController: UITableViewController, UISearchResultsUpdating {
+
+    var filteredTableData = [[AppKey.quesKey : "" , AppKey.ansKey : ""]]
+    var resultSearchController = UISearchController()
+    var selectedIndex : IndexPath? = nil
+    var quesArr=[String]()
+
     let data =
         [
+            // [AppKey.quesKey : "Lorem ipsum dolor sit amet, consectetur adipiscing elit mauris lobortis ornare?" , AppKey.ansKey : "Lorem ipsum dolor sit amet, consectetur adipiscing elit mauris lobortis ornare?Lorem ipsum dolor sit amet, consectetur adipiscing elit mauris lobortis ornare. Lorem ipsum dolor sit amet, consectetur adipiscing elit mauris lobortis ornare."],
+            
+            [AppKey.quesKey : "ABCDE" , AppKey.ansKey : "Lorem ipsum dolor sit amet, consectetur adipiscing elit mauris lobortis ornare?Lorem ipsum dolor sit amet, consectetur adipiscing elit mauris lobortis ornare. Lorem ipsum dolor sit amet, consectetur adipiscing elit mauris lobortis ornare."],
+
+            [AppKey.quesKey : "FGHIJ" , AppKey.ansKey : "Lorem ipsum dolor sit amet, consectetur adipiscing elit mauris lobortis ornare?Lorem ipsum dolor sit amet, consectetur adipiscing elit mauris lobortis ornare. Lorem ipsum dolor sit amet, consectetur adipiscing elit mauris lobortis ornare."],
+
+            [AppKey.quesKey : "KLMNO" , AppKey.ansKey : "Lorem ipsum dolor sit amet, consectetur adipiscing elit mauris lobortis ornare?Lorem ipsum dolor sit amet, consectetur adipiscing elit mauris lobortis ornare. Lorem ipsum dolor sit amet, consectetur adipiscing elit mauris lobortis ornare.Lorem ipsum dolor sit amet, consectetur adipiscing elit mauris lobortis ornare?Lorem ipsum dolor sit amet, consectetur adipiscing elit mauris lobortis ornare. Lorem ipsum dolor sit amet, consectetur adipiscing elit mauris lobortis ornare."],
+
+            [AppKey.quesKey : "PQRST" , AppKey.ansKey : "Lorem ipsum dolor sit amet, consectetur adipiscing elit mauris lobortis ornare?Lorem ipsum dolor sit amet, consectetur adipiscing elit mauris lobortis ornare. Lorem ipsum dolor sit amet, consectetur adipiscing elit mauris lobortis ornare.Lorem ipsum dolor sit amet, consectetur adipiscing elit mauris lobortis ornare?Lorem ipsum dolor sit amet, consectetur adipiscing elit mauris lobortis ornare. Lorem ipsum dolor sit amet, consectetur adipiscing elit mauris lobortis ornare."]
         
-        [AppKey.quesKey : "Lorem ipsum dolor sit amet, consectetur adipiscing elit mauris lobortis ornare?" , AppKey.ansKey : "Lorem ipsum dolor sit amet, consectetur adipiscing elit mauris lobortis ornare?Lorem ipsum dolor sit amet, consectetur adipiscing elit mauris lobortis ornare. Lorem ipsum dolor sit amet, consectetur adipiscing elit mauris lobortis ornare."],
-                
-    [AppKey.quesKey : "Lorem ipsum dolor sit amet, consectetur adipiscing elit mauris lobortis ornare?" , AppKey.ansKey : "Lorem ipsum dolor sit amet, consectetur adipiscing elit mauris lobortis ornare?Lorem ipsum dolor sit amet, consectetur adipiscing elit mauris lobortis ornare. Lorem ipsum dolor sit amet, consectetur adipiscing elit mauris lobortis ornare."],
-    
-    [AppKey.quesKey : "Lorem ipsum dolor sit amet, consectetur adipiscing elit mauris lobortis ornare?" , AppKey.ansKey : "Lorem ipsum dolor sit amet, consectetur adipiscing elit mauris lobortis ornare?Lorem ipsum dolor sit amet, consectetur adipiscing elit mauris lobortis ornare. Lorem ipsum dolor sit amet, consectetur adipiscing elit mauris lobortis ornare.Lorem ipsum dolor sit amet, consectetur adipiscing elit mauris lobortis ornare?Lorem ipsum dolor sit amet, consectetur adipiscing elit mauris lobortis ornare. Lorem ipsum dolor sit amet, consectetur adipiscing elit mauris lobortis ornare."],
-        
-         [AppKey.quesKey : "Lorem ipsum dolor sit amet, consectetur ?" , AppKey.ansKey : "Lorem ipsum dolor sit amet, consectetur adipiscing elit mauris lobortis ornare?Lorem ipsum dolor sit amet, consectetur adipiscing elit mauris lobortis ornare. Lorem ipsum dolor sit amet, consectetur adipiscing elit mauris lobortis ornare.Lorem ipsum dolor sit amet, consectetur adipiscing elit mauris lobortis ornare?Lorem ipsum dolor sit amet, consectetur adipiscing elit mauris lobortis ornare. Lorem ipsum dolor sit amet, consectetur adipiscing elit mauris lobortis ornare."]
-        
-                ]
-    
-    var selectedIndex : IndexPath? = nil
+        ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        resultSearchController = ({
+            let controller = UISearchController(searchResultsController: nil)
+            controller.searchResultsUpdater = self
+            controller.dimsBackgroundDuringPresentation = false
+            controller.searchBar.sizeToFit()
+            
+            tableView.tableHeaderView = controller.searchBar
+            
+            return controller
+        })()
+        
         self.navigationItem.title = "FAQs"
         tableView.estimatedRowHeight = 44.0;
         tableView.rowHeight = UITableView.automaticDimension;
+        
+        // Reload the table
+        tableView.reloadData()
+        
+        for item in data {
+            let ques = item[AppKey.quesKey]
+            print("ques = \(String(describing: ques))")
+            quesArr.append(ques!)
+        }
+        print("quesArr = \(quesArr)")
     }
 
     // MARK: - Table view data source
@@ -46,15 +72,16 @@ class FAQViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // return the number of rows
+        if  (resultSearchController.isActive) {
+            return filteredTableData.count
+        }
         return data.count
+        
+//        return data.count
     }
     
-    
-    
-    
-    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath)-> UITableViewCell {
-        
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "FAQTableViewCell")
         let quesLabel = cell!.viewWithTag(ViewTags.quesT) as! UILabel
@@ -62,6 +89,24 @@ class FAQViewController: UITableViewController {
         let iconLabel = cell!.viewWithTag(ViewTags.plusBtnT) as! UILabel
         let ansBackView = cell!.viewWithTag(ViewTags.ansCoantinerT)!
 
+        
+        if (resultSearchController.isActive) {
+            quesLabel.text = filteredTableData[indexPath.row][AppKey.quesKey]
+            if let index = selectedIndex{
+                if indexPath == index{
+                    ansBackView.isHidden = false
+                    ansLabel.text = filteredTableData[indexPath.row][AppKey.ansKey]
+                    iconLabel.text = "-"
+                    return cell!
+                }
+            }
+            
+            ansLabel.text = ""
+            iconLabel.text = "+"
+            ansBackView.isHidden = true
+            return cell!
+        }
+        
         
         quesLabel.text = data[indexPath.row][AppKey.quesKey]
         if let index = selectedIndex{
@@ -73,18 +118,16 @@ class FAQViewController: UITableViewController {
             }
         }
 
-
         ansLabel.text = ""
         iconLabel.text = "+"
         ansBackView.isHidden = true
         return cell!
-
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
         tableView.deselectRow(at: indexPath, animated: true)
 
-        
         guard let currentCell = tableView.cellForRow(at: indexPath)  else {
             return
         }
@@ -92,7 +135,6 @@ class FAQViewController: UITableViewController {
         let iconLabel = currentCell.viewWithTag(ViewTags.plusBtnT) as! UILabel
         let ansBackView = currentCell.viewWithTag(ViewTags.ansCoantinerT)!
 
-        
         tableView.beginUpdates()
         ansBackView.isHidden = !(ansBackView.isHidden)
         
@@ -114,5 +156,31 @@ class FAQViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
 
         return UITableView.automaticDimension
+    }
+    
+    // MARK: - Search Results Updating Delegate method
+
+    func updateSearchResults(for searchController: UISearchController) {
+        filteredTableData.removeAll(keepingCapacity: false)
+        
+        let searchPredicate = NSPredicate(format: "SELF CONTAINS[c] %@", searchController.searchBar.text!)
+        let array = (quesArr as NSArray).filtered(using: searchPredicate)
+        
+        for item in data {
+            let ques = item[AppKey.quesKey]
+//            print("ques = \(String(describing: ques))")
+            if array.contains(obj: ques){
+                filteredTableData.append(item)
+            }
+        }
+        print("filteredTableData = \(filteredTableData)")
+
+        self.tableView.reloadData()
+    }
+}
+
+extension Array {
+    func contains<T>(obj: T) -> Bool where T : Equatable {
+        return self.filter({$0 as? T == obj}).count > 0
     }
 }
