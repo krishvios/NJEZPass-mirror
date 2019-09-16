eeaf697a4b80d90fca4d6615553f026d6d85aae2
A N S Krishna Vivek <vivek.amirapu@conduent.com>
2019-09-16 17:23:51 +0530

    Added Forgot password UI Changes

---
+++ NJEZPass/NJEZPass/Presentation/Modules/FAQs/FAQConfigurator.swift
@@ -0,0 +1,22 @@
+//
+//  FAQConfigurator.swift
+//  NJEZPass
+//
+//  Created by Amirapu, Vivek (External) on 12/09/19.
+//  Copyright © 2019 Conduent. All rights reserved.
+//
+
+import Foundation
+class FAQConfigurator {
+    
+    var interactor: FAQInteractor?
+    var presenter: FAQPresenter?
+    var router: LoginRouter?
+    //    func build(viewController: EditProfileViewController) {
+    //        interactor = EditProfileInteractor()
+    //        presenter = LoginPresenter()
+    //        router = LoginRouter(delegate: viewController)
+    //        interactor?.presenter = presenter
+    //        presenter?.viewController = viewController
+    //    }
+}
