b75513cf9b4615cc5bfabad3139f94153cd90ccc
Narasimha <>
2019-09-23 13:17:17 +0530

    Paybill flow integrated

---
+++ NJEZPass/NJEZPass/Presentation/Modules/PayViolation/SelectPaymentVC.swift
@@ -0,0 +1,21 @@
+//
+//  SelectPaymentVC.swift
+//  NJEZPass
+//
+//  Created by N, Narasimhulu on 18/09/19.
+//  Copyright © 2019 Conduent. All rights reserved.
+//
+
+import UIKit
+
+class SelectPaymentVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func backButtonTapped(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
+    }
+}
---
+++ NJEZPass/NJEZPass/Presentation/Modules/PayViolation/SignupAndSaveVC.swift
@@ -0,0 +1,21 @@
+//
+//  SignupAndSaveVC.swift
+//  NJEZPass
+//
+//  Created by N, Narasimhulu on 18/09/19.
+//  Copyright © 2019 Conduent. All rights reserved.
+//
+
+import UIKit
+
+class SignupAndSaveVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func backButtonTapped(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
+    }
+}
