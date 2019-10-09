b437eaead9c4afb65006a9ca4d5ebe67014642cb
Arshad Alam <sk.alam@conduent.com>
2019-10-09 12:57:55 +0530

    vehicle cell added

--- NJEZPass/NJEZPass/Presentation/Modules/Vehicles/VehiclesViewController.swift
+++ NJEZPass/NJEZPass/Presentation/Modules/Vehicles/VehiclesViewController.swift
@@ -63,7 +63,11 @@ extension VehiclesViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "\(VehicleCell.self)", for: indexPath) as! VehicleCell
+       // let cell = tableView.dequeueReusableCell(withIdentifier: "\(VehicleCell.self)", for: indexPath) as! VehicleCell
+        let cell : VehicleListTableViewCell = tableView.dequeueReusableCell(withIdentifier: "\(VehicleListTableViewCell.self)", for: indexPath) as! VehicleListTableViewCell
+
+        
+       // VehicleListTableViewCell
         //set the cell display properties
        // cell.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.1).cgColor
        // cell.layer.shadowOffset = CGSize(width: 0.0, height: 1.0)
